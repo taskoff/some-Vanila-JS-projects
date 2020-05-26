18dc19dcb01fea67d8721a4e14b5ba283bab47e8
taskoff <marian_taskov@abv.bg>
2020-05-26 14:42:43 +0300

    add functionality

--- simple-calculator/script.js
+++ simple-calculator/script.js
@@ -8,40 +8,50 @@ elements.calc.addEventListener('click', getElement)
 
 let fn = '';
 let sn = '';
-const types = {
-    digit: (n) => {
-        if (n==='.' && fn === '') {
-            fn = '0'
-        }
-        fn += n
-        },
-    clear: () => {
-        fn = '';
-    },
-    operation: (v) =>{
-        if (v==='+') {
-            if (fn && !sn) {
-                sn = fn;
-                fn = ''
-             } 
-
-        } else if (v==='=') {
-            if (fn && sn) {
-                fn = Number(fn) + Number(sn);
-            }    
-        }
-    }
+let operator = '';
+let result = '';
+const operators = {
+    '+': (a,b)=>{return Number(a)+Number(b);},
+    '-': (a,b)=>{return Number(b)-Number(a)},
+    '/': (a,b)=>{return Number(b)/Number(a);},
+    '*': (a,b)=>{return Number(b)*Number(a);},
     
 }
+
+const types = {
+    digit: (n) => {
+        if (n==='.' && fn === '') {fn = '0'}
+        fn += n
+        },
+    clear: () => {fn = '';},
+    operation: o => {
+        operator = o
+        if (fn) {
+            sn = fn;
+            fn = ''
+        }
+    },
+    'equal': makeOperation
+}
+    
+    
+
 function getElement(e) {
     let type = e.target.getAttribute('class');
     let value = e.target.value;
     types[type](value);
+
     addNum(fn)
 }
 
 function addNum(d) {
     elements.output.textContent = d;
-    console.log(d)
+}
+
+function makeOperation(){
+    if(fn && sn) {
+        result = operators[operator](fn, sn);
+        fn = result
+     }
 }
 
