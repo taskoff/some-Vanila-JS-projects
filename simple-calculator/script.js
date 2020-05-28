c1aff5a306eafb9a51e99b877e13a8859ec90edd
taskoff <marian_taskov@abv.bg>
2020-05-28 11:27:05 +0300

    add limit length of number

--- simple-calculator/script.js
+++ simple-calculator/script.js
@@ -11,7 +11,8 @@ const staff = {
     sn: '',
     operator: '',
     result: '',
-    numForDispaly: 0
+    numForDispaly: 0,
+    symbols: ['+', '-', '/', '*']
 }
 const operators = {
     '+': (a,b)=>{return Number(a)+Number(b);},
@@ -23,7 +24,9 @@ const operators = {
 
 const types = {
     digit: (n) => {
-        staff.fn.push(n);
+        if (staff.fn.length<10) {
+            staff.fn.push(n);
+        }
         if (staff.fn[0] === 0 && staff.fn[1] !== '.') {
             staff.fn.shift()
         }       
@@ -40,7 +43,7 @@ const types = {
             staff.numForDispaly = staff.fn;
             staff.fn = [0];
         } else if(staff.fn !== 0 && staff.sn !== '') {
-            makeOperation()
+            makeOperation(staff.operator)
             staff.operator = o;
         }
     },
@@ -51,37 +54,46 @@ const types = {
 
 function getElement(e) {
     let type = e.target.getAttribute('class');
-    let value = e.target.value;
+    let value = e.target.value !== '='? e.target.value : staff.operator;
     types[type](value);
 
     addNum(staff.numForDispaly)
 }
 
 function addNum(d) {
+    console.log(d);
+    if (d[0].length>10) {
+        d[0]=d[0].substr(0,10)
+    }
     elements.output.textContent = d.join('');
 }
 
-function makeOperation(){
+function makeOperation(o){
     if(staff.fn && staff.sn) {
         let a = Number(staff.sn.join(''));
         let b = Number(staff.fn.join(''));
-        staff.sn = operators[staff.operator](b, a).toString().split();
+        staff.sn = operators[o](b, a).toString().split();
         staff.numForDispaly = staff.sn;
         staff.fn = [0]
      }
 }
-const symbols = ['+', '-', '/', '*']
+
 function keyHandler(e) {
     
-    if (e.key == Number(e.key)) {
+    if (e.key == Number(e.key) || e.key === '.') {
         let value = e.key;
         types['digit'](value);
         addNum(staff.numForDispaly)
-        
-    } else if (symbols.includes(e.key)) {
+    } else if (staff.symbols.includes(e.key)) {
+        staff.operator = e.key;
         types.operation(e.key);
+    } else if (e.key === 'Enter') {
+        makeOperation(staff.operator);
+        addNum(staff.sn)
+    } else if (e.key === 'c') {
+        types.clear()
+        addNum(staff.numForDispaly)
     }
-    console.log(e.key)
-    // console.log(e.key)
+  
 }
 
